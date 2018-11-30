93afffe2a9e500d842b55da671590b83469ce040
Karthik Praturu <kart.prat@gmail.com>
2018-11-30 12:16:01 -0500

    Added blending and median filtering. Was testing penalizing optical flow, but TF stopped recognizing my GPU.

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,7 @@ import transform, numpy as np, vgg, pdb, os
 import scipy.misc
 import tensorflow as tf
 from utils import save_img, get_img, exists, list_files
+from scipy import ndimage
 from scipy.ndimage.filters import gaussian_filter
 from argparse import ArgumentParser
 from collections import defaultdict
@@ -47,7 +48,7 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
                 raise Exception("No checkpoint found...")
         else:
             saver.restore(sess, checkpoint_dir)
-        
+
         X = np.zeros(batch_shape, dtype=np.float32)
         frame_count = 0  # The frame count that written to X
         curFrame = np.zeros(batch_shape[1:])
@@ -70,7 +71,7 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
                 if not prevStyledFrame.any():
                     # print("inital")
                     video_writer.write_frame(newStyledFrame)
-                    
+
                 else:
                     nxt = cv.cvtColor(curFrame,cv.COLOR_BGR2GRAY)
                     prv = cv.cvtColor(prevFrame,cv.COLOR_BGR2GRAY)
@@ -83,13 +84,29 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
                     threshold = 25
                     sigChange = normalizedMag > threshold
 
-                    # plt.imshow(sigChange)
-                    # plt.show()
-
+                    #plt.imshow(sigChange.astype(np.double))
+                    #plt.show()
 
                     newStyledFrame[np.logical_not(sigChange)] = prevStyledFrame[np.logical_not(sigChange)]
 
-                    
+                    defaultStyledFrame = np.clip(_preds[i], 0, 255).astype(np.uint8)
+
+                    # Penalizing pixels with large optical flow by choosing the
+                    # default style frame instead
+                    penalties = normalizedMag / np.max(normalizedMag)
+                    penalties = np.repeat(penalties[:,:,np.newaxis], 3, axis=2)
+                    newStyledFrame = (newStyledFrame*(1-penalties) + defaultStyledFrame*(penalties)).astype(np.uint8)
+
+                    #newStyledFrame = (newStyledFrame*0.5 + defaultStyledFrame*0.5).astype(np.uint8) # Averaging images together
+
+                    # Treating all the dots as salt and pepper and Gaussian noise.
+                    # Best filter to use seems to be a median filter
+                    newStyledFrame[:, :, 0] = ndimage.median_filter(newStyledFrame[:, :, 0], 5)
+                    newStyledFrame[:, :, 1] = ndimage.median_filter(newStyledFrame[:, :, 1], 5)
+                    newStyledFrame[:, :, 2] = ndimage.median_filter(newStyledFrame[:, :, 2], 5)
+
+                    #plt.imshow(newStyledFrame)
+                    #plt.show()
 
                     # print("other")
                     #diff = curFrame - prevFrame.astype(np.uint8)
@@ -107,10 +124,10 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
                     # input()
                     video_writer.write_frame(newStyledFrame)
 
-            return np.clip(_preds[i], 0, 255).astype(np.uint8)
-            # return newStyledFrame
-        
-        
+            #return np.clip(_preds[i], 0, 255).astype(np.uint8)
+            return newStyledFrame
+
+
         for frame in video_clip.iter_frames():
             if not prevFrame.any() and not curFrame.any():
                 X[frame_count] = frame
@@ -119,10 +136,10 @@ def ffwd_video(path_in, path_out, checkpoint_dir, device_t='/gpu:0', batch_size=
                 prevFrame = curFrame.copy()
                 curFrame = frame.copy()
                 X[frame_count] = curFrame.copy()
-            
+
             frame_count += 1
             if frame_count == batch_size:
-                
+
                 prevStyledFrame = style_and_write(frame_count, prevStyledFrame, prevFrame, curFrame)
                 frame_count = 0
 
@@ -183,18 +200,18 @@ def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
             _preds = sess.run(preds, feed_dict={img_placeholder:X})
             for j, path_out in enumerate(curr_batch_out):
                 save_img(path_out, _preds[j])
-                
+
         remaining_in = data_in[num_iters*batch_size:]
         remaining_out = paths_out[num_iters*batch_size:]
     if len(remaining_in) > 0:
-        ffwd(remaining_in, remaining_out, checkpoint_dir, 
+        ffwd(remaining_in, remaining_out, checkpoint_dir,
             device_t=device_t, batch_size=1)
 
 def ffwd_to_img(in_path, out_path, checkpoint_dir, device='/cpu:0'):
     paths_in, paths_out = [in_path], [out_path]
     ffwd(paths_in, paths_out, checkpoint_dir, batch_size=1, device_t=device)
 
-def ffwd_different_dimensions(in_path, out_path, checkpoint_dir, 
+def ffwd_different_dimensions(in_path, out_path, checkpoint_dir,
             device_t=DEVICE, batch_size=4):
     in_path_of_shape = defaultdict(list)
     out_path_of_shape = defaultdict(list)
@@ -206,7 +223,7 @@ def ffwd_different_dimensions(in_path, out_path, checkpoint_dir,
         out_path_of_shape[shape].append(out_image)
     for shape in in_path_of_shape:
         print('Processing images of shape %s' % shape)
-        ffwd(in_path_of_shape[shape], out_path_of_shape[shape], 
+        ffwd(in_path_of_shape[shape], out_path_of_shape[shape],
             checkpoint_dir, device_t, batch_size)
 
 def build_parser():
@@ -234,7 +251,7 @@ def build_parser():
                         metavar='BATCH_SIZE', default=BATCH_SIZE)
 
     parser.add_argument('--allow-different-dimensions', action='store_true',
-                        dest='allow_different_dimensions', 
+                        dest='allow_different_dimensions',
                         help='allow different image dimensions')
 
     return parser
@@ -265,7 +282,7 @@ def main():
         full_in = [os.path.join(opts.in_path,x) for x in files]
         full_out = [os.path.join(opts.out_path,x) for x in files]
         if opts.allow_different_dimensions:
-            ffwd_different_dimensions(full_in, full_out, opts.checkpoint_dir, 
+            ffwd_different_dimensions(full_in, full_out, opts.checkpoint_dir,
                     device_t=opts.device, batch_size=opts.batch_size)
         else :
             ffwd(full_in, full_out, opts.checkpoint_dir, device_t=opts.device,
